1fe0f7bbe76315e43b8aec76b691d9cc0a6851b4
RickDev-O <85527039+RickDev-O@users.noreply.github.com>
2022-07-27 18:00:49 -0300

    Update userlogin.py

--- userlogin.py
+++ userlogin.py
@@ -4,8 +4,8 @@ os.system("clear")
 print("WorldOS Login")
 data1 = open("name", "r")
 data2 = open("code", "r")
-usrdata = data1.read()
-passdata = data2.read()
+usrdata = data2.read()
+passdata = data1.read()
 print("Username: "+passdata)
 ok = 0
 while True:
